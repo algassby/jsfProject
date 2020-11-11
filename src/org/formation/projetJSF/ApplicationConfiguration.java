8e779355d18417b29774e348645e49bc691b890a
algassby <algassby@hotmail.com>
2020-11-11 14:19:17 +0100

    JSF project

---
+++ src/org/formation/projetJSF/ApplicationConfiguration.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.formation.projetJSF;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+@ApplicationScoped
+@FacesConfig ( version = FacesConfig.Version.JSF_2_3 )  
+
+/**
+ * @author algas
+ *
+ */
+public class ApplicationConfiguration {
+
+	
+
+}
